d885f170a5d37e931f79275a7febdbf293be9c48
OSDVF <o.s.dv.f@seznam.cz>
2024-11-08 17:48:04 +0100

    Add icon load error message

--- src/wv/webview-windows.h
+++ src/wv/webview-windows.h
@@ -238,6 +238,20 @@ public:
     if (hIcon)
     {
       SendMessage(m_window, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+    } else {
+      // https://stackoverflow.com/a/17387176
+      auto error = GetLastError();
+      if (error != 0) {
+        LPSTR messageBuffer = nullptr;
+
+        //Ask Win32 to give us the string version of that message ID.
+        //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
+        size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                    NULL, error, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+        fprintf(stderr, "Failed to load icon: %s\n", messageBuffer);
+        //Free the Win32's string's buffer.
+        LocalFree(messageBuffer);
+      }
     }
   }
 
